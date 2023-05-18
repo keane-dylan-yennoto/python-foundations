974b73776da93c0bd4a62b0568022134cdfb95e0
keane-dylan-yennnoto <keanedylan.yennoto@outlook.com>
2023-05-18 22:16:23 +0800

    v2

---
+++ excersices/1 Introduction to Python/1 Store Calculator/1 Store Calculator.py
@@ -0,0 +1,14 @@
+""" Exercise 1: Store Calculator """
+
+''' 
+Write a program that acts as a simple store calculator. 
+
+The program should take the name, price, quantity, and discount percentage of an item from the user 
+
+You will need to calculate the total price and the total price after applying the discount. 
+Both prices should be printed to the nearest second decimal place 
+
+See the problem output in the '1 Store Calculator Challenge.txt'
+'''
+
+# Code Here
